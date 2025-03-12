4e6b147475454566babf0b691ca8652cb9285069
Maike <maikonalexandre574@gmail.com>
2025-03-12 09:38:56 -0300

    fix: update environment variable MODE to use 'production' instead of 'prod'

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  MODE: z.enum(["dev", "prod", "test"]).default("dev"),
+  MODE: z.enum(["dev", "production", "test"]).default("dev"),
   VITE_API_URL: z.string().default("http://localhost:4000"),
   VITE_SERVICE_VERSION: z.string().default("1.0.0"),
 });
