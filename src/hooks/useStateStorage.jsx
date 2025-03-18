27f1bc1a9fbe6a454488001f31a20899b1385bf2
Maike <maikonalexandre574@gmail.com>
2025-03-18 10:11:24 -0300

    fix: correct default value parsing in useStateWithStorage hook

--- src/hooks/useStateStorage.jsx
+++ src/hooks/useStateStorage.jsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 
 export const useStateWithStorage = (STORAGE_KEY) => {
   const defaultValue = localStorage.getItem(STORAGE_KEY);
-  const [value, setValue] = useState(JSON.parse(defaultValue ?? ""));
+  const [value, setValue] = useState(JSON.parse(defaultValue) ?? "");
 
   const updateValue = useCallback(
     (value) => {
