b1b3d929ddf9f7b27eab5ea4fddbee9b6c201415
Maike <maikonalexandre574@gmail.com>
2025-03-28 15:58:07 -0300

    feat: add formatDateToDDMMYYYY utility function for date formatting

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -4,3 +4,10 @@ export const formatDate = (date, pattern = "dd/MM/yyyy") => {
   if (!date) return "";
   return format(new Date(date), pattern);
 };
+
+export const formatDateToDDMMYYYY = (date) => {
+  if (!date) return "";
+  const [year, month, day] = date.split("T")[0].split("-");
+
+  return `${day}/${month}/${year}`;
+};
