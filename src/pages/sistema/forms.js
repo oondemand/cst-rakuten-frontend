d519bcdccfabcfda1876f5a2abdd3ff405c3d141
Maike <maikonalexandre574@gmail.com>
2025-04-01 14:36:37 -0300

    feat: refactor Sistema page to use dynamic forms and remove Sci Unico component

---
+++ src/pages/sistema/forms.js
@@ -0,0 +1,110 @@
+import { DefaultField } from "../../components/buildForm/filds/default";
+import { DateField } from "../../components/buildForm/filds/dateField";
+import { z } from "zod";
+
+export const FORMS = [
+  {
+    title: "Sci Unico",
+    fields: [
+      {
+        accessorKey: "sci.codigo_empresa",
+        label: "Código empresa",
+        render: DefaultField,
+        validation: z.string().min(1, "Codigo empresa é um campo obrigatório."),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.codigo_centro_custo",
+        label: "Código centro custo",
+        render: DefaultField,
+        validation: z
+          .string()
+          .min(1, "Código centro custo é um campo obrigatório."),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.porcentagem_iss",
+        label: "Porcentagem iss",
+        render: DefaultField,
+        validation: z.coerce
+          .number({
+            invalid_type_error: "Porcentagem iss tem que ser um número.",
+          })
+          .optional(),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.dias_pagamento",
+        label: "Dias de pagamento",
+        render: DefaultField,
+        validation: z.string().min(1, "Dias pagamento é um campo obrigatório!"),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.cbo",
+        label: "CBO",
+        render: DefaultField,
+        validation: z.string().min(1, "CBO é um campo obrigatório."),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.cfip",
+        label: "CFIP",
+        render: DefaultField,
+        validation: z.string().min(1, "CFIP é um campo obrigatório."),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "sci.e_social",
+        label: "E-SOCIAL",
+        render: DefaultField,
+        validation: z.string().min(1, "E-SOCIAL é um campo obrigatório."),
+        colSpan: 1,
+      },
+    ],
+  },
+  {
+    title: "Omie",
+    fields: [
+      {
+        accessorKey: "omie.id_conta_corrente",
+        label: "Id conta corrente",
+        render: DefaultField,
+        validation: z
+          .string()
+          .nonempty("Id conta corrente é um campo obrigatório."),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "omie.codigo_categoria",
+        label: "Codigo categoria",
+        render: DefaultField,
+        validation: z
+          .string()
+          .nonempty("Código categoria é um campo obrigatório"),
+        colSpan: 1,
+      },
+    ],
+  },
+  {
+    title: "Geral",
+    fields: [
+      {
+        accessorKey: "remetente",
+        label: "Remetente",
+        render: DefaultField,
+        validation: z.string().email("Email inválido!").nonempty(),
+        colSpan: 1,
+      },
+      {
+        accessorKey: "data_corte_app_publisher",
+        label: "Data de corte app publisher",
+        render: DateField,
+        validation: z
+          .string()
+          .nonempty("Código categoria é um campo obrigatório."),
+        colSpan: 1,
+      },
+    ],
+  },
+];
--- src/pages/sistema/index.jsx
+++ src/pages/sistema/index.jsx
@@ -1,28 +1,39 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { SciUnico } from "./form/sci-unico";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { FORMS } from "./forms";
+import { BuildForm } from "../../components/buildForm";
 
 export const SistemaPage = () => {
+  const forms = useMemo(() => FORMS, []);
+
   return (
     <Flex flex="1" flexDir="column" py="8" px="6" bg="#F8F9FA" overflow="auto">
-      <Flex flexWrap="wrap" gap="8">
-        <Box shadow="xs" p="6" rounded="lg" bg="white">
-          <Text color="gray.500" fontWeight="medium">
-            Sci Unico
-          </Text>
-          <SciUnico />
-        </Box>
-
-        <Box shadow="xs" p="6" rounded="lg" bg="white">
-          <Text color="gray.500" fontWeight="medium">
-            Omie
-          </Text>
-        </Box>
-
-        <Box shadow="xs" p="6" rounded="lg" bg="white">
-          <Text color="gray.500" fontWeight="medium">
-            Geral
-          </Text>
-        </Box>
+      <Text fontWeight="semibold" fontSize="lg">
+        Configurações de sistema
+      </Text>
+      <Flex mt="4" flexWrap="wrap" gap="8">
+        {forms.map((form) => (
+          <Box key={form.title}>
+            <Box shadow="xs" p="6" pb="8" rounded="lg" bg="white">
+              <Text color="gray.500" fontWeight="medium">
+                {form.title}
+              </Text>
+              <Box mt="6">
+                <BuildForm
+                  gridColumns={2}
+                  onSubmit={(value) => console.log("LOG", value)}
+                  fields={form.fields}
+                  gap={6}
+                />
+                {form.title === "Geral" && (
+                  <Button variant="surface" mt="8">
+                    Testar envio de email
+                  </Button>
+                )}
+              </Box>
+            </Box>
+          </Box>
+        ))}
       </Flex>
     </Flex>
   );
