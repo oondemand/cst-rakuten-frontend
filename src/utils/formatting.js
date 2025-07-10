53ed709cc15d1f22039a8b6acd78b528f574b995
Maike <maikonalexandre574@gmail.com>
2025-07-10 15:04:38 -0300

    feat: add CNPJ and CPF formatting functions and update formatPrestadorLabel for document display

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -12,10 +12,39 @@ export const formatDateToDDMMYYYY = (date) => {
   return `${day}/${month}/${year}`;
 };
 
+export function formatarCNPJ(documento) {
+  // Remove tudo que não é dígito
+  const num = documento.replace(/\D/g, "");
+
+  // Aplica a máscara do CNPJ
+  return num.replace(
+    /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/,
+    "$1.$2.$3/$4-$5"
+  );
+}
+
+export function formatarCPF(documento) {
+  // Remove tudo que não é dígito
+  const num = documento.replace(/\D/g, "");
+
+  // Aplica a máscara do CPF
+  return num.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, "$1.$2.$3-$4");
+}
+
 export const formatPrestadorLabel = (prestador) => {
   if (!prestador) return "";
 
+  let documento = prestador.documento;
+
+  if (prestador?.tipo === "pf") {
+    documento = formatarCPF(prestador.documento);
+  }
+
+  if (prestador?.tipo === "pj") {
+    documento = formatarCNPJ(prestador.documento);
+  }
+
   return `${prestador.nome} ${
     prestador.sid ? `- SID: ${prestador.sid?.join(";")}` : ""
-  }  ${prestador.documento ? `- DOC: ${prestador.documento}` : ""}`;
+  }  ${documento ? `- DOC: ${documento}` : ""}`;
 };
