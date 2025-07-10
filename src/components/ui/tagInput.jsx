9c7e5da4c7dbd962b851315c9f6b7bcc81c3f61a
Maike <maikonalexandre574@gmail.com>
2025-07-10 10:35:36 -0300

    feat: add TagInput component for managing tags with input and removal functionality

---
+++ src/components/ui/tagInput.jsx
@@ -0,0 +1,66 @@
+import { forwardRef, useCallback, useRef } from "react";
+import { Tag, Input, Wrap, WrapItem, Text, Box } from "@chakra-ui/react";
+import { X } from "lucide-react";
+
+export const TagInput = ({
+  tags = [],
+  onTagsChange,
+  onAddTag,
+  keys = ["Enter"],
+}) => {
+  const inputRef = useRef(null);
+
+  const addTags = (event, tag) => {
+    onAddTag?.(event, tag);
+    if (event.defaultPrevented) return;
+    onTagsChange?.(event, [...tags, tag]);
+  };
+
+  const removeTag = (event, index) => {
+    const newTags = [...tags];
+    newTags.splice(index, 1);
+    onTagsChange?.(event, newTags);
+    inputRef.current.focus();
+  };
+
+  const handleKeyDown = (event) => {
+    const { key, currentTarget } = event;
+    const { value, selectionStart, selectionEnd } = currentTarget;
+
+    if (keys.includes(key) && value) {
+      addTags(event, value);
+      if (!event.defaultPrevented) currentTarget.value = "";
+      event.preventDefault();
+    }
+
+    const inputIsClean = selectionStart === 0 && selectionEnd === 0;
+    if (key === "Backspace" && tags.length > 0 && inputIsClean) {
+      removeTag(event, tags.length - 1);
+    }
+  };
+
+  return (
+    <Wrap alignItems="center">
+      {tags.map((tag, i) => {
+        return (
+          <WrapItem key={i}>
+            <Tag.Root py="1">
+              <Tag.Label>{tag}</Tag.Label>
+              <Tag.EndElement onClick={(e) => removeTag(e, i)}>
+                <X size={14} />
+              </Tag.EndElement>
+            </Tag.Root>
+          </WrapItem>
+        );
+      })}
+      <WrapItem flexGrow="1">
+        <Input
+          ref={inputRef}
+          placeholder="Digite e pressione enter"
+          variant="flushed"
+          onKeyDown={handleKeyDown}
+        />
+      </WrapItem>
+    </Wrap>
+  );
+};
